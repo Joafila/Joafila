8da7587925259818159fa488855f56b764ea7065
joafila <joafila@edot-solutions.com>
2023-11-30 11:08:02 +0530

    forgot password

--- pages/ForgotPassword.py
+++ pages/ForgotPassword.py
@@ -9,23 +9,23 @@ class ForgotPasswordPageClass:
     proceed_confirmation_no_btn = "//button[normalize-space()='No']"
     forgot_password_error_message_id = 'invalid-login-email-message'
 
-    # def __init__(self, driver):
-    #     self.driver = driver
-    #
-    # def clickForgotPassword(self):
-    #     self.driver.find_element(By.LINK_TEXT, self.forgot_password).click()
-    #
-    # def enterRegisteredEmail(self, email):
-    #     self.driver.find_element(By.ID, self.email).send_keys(email)
-    #
-    # def clickRequestNewPassword(self):
-    #     self.driver.find_element(By.CSS_SELECTOR, self.request_new_password).click()
-    #
-    # def clickConfirmationYes(self):
-    #     self.driver.find_element(By.CSS_SELECTOR,self.proceed_confirmation_yes_btn).click()
-    #
-    # def clickConfirmationNo(self):
-    #     self.driver.find_element(By.XPATH,self.proceed_confirmation_no_btn).click()
-    #
-    # def errorToContactAdmin(self):
-    #     return self.driver.find_element(By.CLASS_NAME, self.forgot_password_error_message_id).text
+    def __init__(self, driver):
+        self.driver = driver
+
+    def clickForgotPassword(self):
+        self.driver.find_element(By.LINK_TEXT, self.forgot_password).click()
+
+    def enterRegisteredEmail(self, email):
+        self.driver.find_element(By.ID, self.email).send_keys(email)
+
+    def clickRequestNewPassword(self):
+        self.driver.find_element(By.CSS_SELECTOR, self.request_new_password).click()
+
+    def clickConfirmationYes(self):
+        self.driver.find_element(By.CSS_SELECTOR,self.proceed_confirmation_yes_btn).click()
+
+    def clickConfirmationNo(self):
+        self.driver.find_element(By.XPATH,self.proceed_confirmation_no_btn).click()
+
+    def errorToContactAdmin(self):
+        return self.driver.find_element(By.CLASS_NAME, self.forgot_password_error_message_id).text
--- tests/test_forgot_password.py
+++ tests/test_forgot_password.py
@@ -1,21 +1,21 @@
 import pytest
 
 from pages.ForgotPassword import ForgotPasswordPageClass
-# from pages.Login import LoginPageClass
-#
-#
-# @pytest.mark.usefixtures("set_up_tear_down")
-# class TestForgotPassword:
-#     driver = None
-#
-#     def enter_registered_email(self):
-#         login_page = LoginPageClass(self.driver)
-#         login_page.clickSignIn()
-#         forgot_password = ForgotPasswordPageClass(self.driver)
-#         forgot_password.clickForgotPassword()
-#         forgot_password.enterRegisteredEmail('testlateredot@gmail.com')
-#         forgot_password.clickRequestNewPassword()
-#         forgot_password.clickConfirmationYes()
-#         expected_error_msg = 'Please contact the eDOT Team to change the Admin password.'
-#         assert forgot_password.errorToContactAdmin().__eq__(expected_error_msg)
+from pages.Login import LoginPageClass
+
+
+@pytest.mark.usefixtures("set_up_tear_down")
+class TestForgotPassword:
+    driver = None
+
+    def enter_registered_email(self):
+        login_page = LoginPageClass(self.driver)
+        login_page.clickSignIn()
+        forgot_password = ForgotPasswordPageClass(self.driver)
+        forgot_password.clickForgotPassword()
+        forgot_password.enterRegisteredEmail('testlateredot@gmail.com')
+        forgot_password.clickRequestNewPassword()
+        forgot_password.clickConfirmationYes()
+        expected_error_msg = 'Please contact the eDOT Team to change the Admin password.'
+        assert forgot_password.errorToContactAdmin().__eq__(expected_error_msg)
 
