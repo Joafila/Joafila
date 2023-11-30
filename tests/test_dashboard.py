6f3bffa46d7bcbe5c51b94730dd76d06df20623f
joafila <joafila@edot-solutions.com>
2023-11-30 17:38:29 +0530

    test_dashboard changes

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -1,6 +1,7 @@
 import pytest
 
 from pages.Dashboard import DashboardPageClass
+from pages.Login import LoginPageClass
 
 
 @pytest.mark.usefixtures("set_up_tear_down")
@@ -8,11 +9,21 @@ class TestDashboardPage:
     driver = None
 
     def test_if_header_elements_clickable(self):
+        login_page = LoginPageClass(self.driver)
+        login_page.enterUsername('ctiadmindev')
+        login_page.enterPassword('Admin@12345')
+        login_page.tapTermsAndConditions()
+        login_page.clickSignIn()
         dashboard_page = DashboardPageClass(self.driver)
         dashboard_page.clickBellIcon()
         dashboard_page.clickNotificationCrossIcon()
 
     def test_if_tabs_are_clickable(self):
+        login_page = LoginPageClass(self.driver)
+        login_page.enterUsername('ctiadmindev')
+        login_page.enterPassword('Admin@12345')
+        login_page.tapTermsAndConditions()
+        login_page.clickSignIn()
         dashboard_page = DashboardPageClass(self.driver)
         dashboard_page.clickManageClientShipTab()
         dashboard_page.clickDashboardMenuLink()
