ae8a31dafb84659b4e70978f1b34b44fafaf57d5
joafila <joafila@edot-solutions.com>
2023-11-30 13:08:09 +0530

    adding dashboard and manage user files

--- tests/test_manange_users.py
+++ tests/test_manange_users.py
@@ -2,6 +2,7 @@ import pytest
 
 from pages.Login import LoginPageClass
 from pages.Dashboard import DashboardPageClass
+from pages.ManageUsers import ManageUserClass
 
 
 @pytest.mark.usefixtures("set_up_tear_down")
@@ -16,3 +17,32 @@ class TestManageUsers:
         login_page.clickSignIn()
         dashboard_page = DashboardPageClass(self.driver)
         dashboard_page.clickManageUserTab()
+
+    def test_search_valid_manage_user( self ):
+        login_page = LoginPageClass(self.driver)
+        login_page.enterUsername('ctiadmindev')
+        login_page.enterPassword('Admin@12345')
+        login_page.tapTermsAndConditions()
+        login_page.clickSignIn()
+        dashboard_page = DashboardPageClass(self.driver)
+        dashboard_page.clickManageUserTab()
+        manage_user_page = ManageUserClass(self.driver)
+        manage_user_page.clickSearchBar('Tim')
+
+    def test_search_invalid_manage_user(self):
+        login_page = LoginPageClass(self.driver)
+        login_page.enterUsername('ctiadmindev')
+        login_page.enterPassword('Admin@12345')
+        login_page.tapTermsAndConditions()
+        login_page.clickSignIn()
+        dashboard_page = DashboardPageClass(self.driver)
+        dashboard_page.clickManageUserTab()
+        manage_user_page = ManageUserClass(self.driver)
+        manage_user_page.clickSearchBar('Jim')
+        expected_message = "No Rows To Show"
+        assert manage_user_page.no_records_message().__eq__(expected_message)
+
+
+
+
+
